d97add3e8204d16d4572a0a4e2bc2ce6316a234c
MateuszGolda <mateusz.golda1993@gmail.com>
2020-05-22 17:19:46 +0200

    Initial commit

---
+++ src/main/java/com/codecool/hangman/ImportCountries.java
@@ -0,0 +1,13 @@
+package com.codecool.hangman;
+
+/**
+ * ImportCountries
+ */
+public class ImportCountries {
+
+    public String[][] ReturnArray() {
+        String[] countries = {"a", "b"};
+        String[] capitals = {"c", "d"};
+        return new String[][]{countries, capitals};
+    }
+}
--- src/main/java/com/codecool/hangman/Main.java
+++ src/main/java/com/codecool/hangman/Main.java
@@ -1,10 +1,10 @@
-/**
- * Hangman
- */
-public class Hangman {
+package com.codecool.hangman;
 
+/**
+ * Main
+ */
+public class Main {
     public static void main(String[] args) {
         Display.startScreen();
     }
-
 }
